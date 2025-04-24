617b9c6e130afd374432bacdac85443d4886203c
muhaiminultasin <tasinmuhaiminul@gmail.com>
2025-04-24 15:23:12 +0600

    hero cng

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -10,7 +10,7 @@ const Button = ({name,btnClass}: ButtonProps) => {
 
   return (
     <div>
-      <button className={cn("bg-[#E30B5C] text-white px-4 py-2 rounded mt-5 cursor-pointer",btnClass)}>{name}</button>
+      <button className={cn("bg-[#E30B5C] text-white font-bold px-4 py-2 mt-5 cursor-pointer rounded-full",btnClass)}>{name}</button>
     </div>
   )
 }
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,26 +5,24 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="w-full md:h-screen  px-10 md:px-20  grid grid-cols-1 md:grid-cols-2 items-center gap-10 overflow-hidden">
-      <div className="pt-32">
-        <h1 className="text-5xl md:text-6xl font-bold text-primary">
-          Comfort in every step __
-        </h1>
-        <p className="text-2xl mt-5 font-medium text-[#474646]">
-          With ComfortWalk
-        </p>
-        <p className="mt-3 text-[#474646]">
-          ComfortWalk offers simple, stylish, and comfortable shoes for men and
-          women. Made for everyday steps.
-        </p>
+    <section className="relative overflow-hidden py-32 px-10 md:px-20 rounded-b-3xl">
+      <div className=" absolute inset-0 w-full md:h-screen   items-center gap-10 overflow-hidden text-white bg-[url('https://images.unsplash.com/photo-1600269452121-4f2416e55c28?q=80&w=1000')] bg-cover bg-center opacity-20"></div>
+      <div className="absolute inset-0 bg-[#385195] opacity-90 mix-blend-multiply"></div>
+      <div className="container mx-auto relative z-10">
+        <div className="max-w-xl text-white">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
+            Step into Style – Find Your Perfect Pair
+          </h1>
+
+          <p className="mt-3 text-xl">
+            Trendy, comfy, and made to last. Discover our latest collection of
+            stylish shoes for every step of your journey.
+          </p>
+        </div>
 
         <Button name="Shop Now" btnClass={""} />
       </div>
-
-      <div className="relative w-full h-[380px] md:h-full">
-      <Image src="/images/hero.png" alt="hero" fill />
-      </div>
-    </div>
+    </section>
   );
 };
 
